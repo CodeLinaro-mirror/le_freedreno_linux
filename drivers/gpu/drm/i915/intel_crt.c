46cdb1103d97e37363f0edcbe91d312ceadab28e
Rob Clark <robdclark@gmail.com>
2014-06-04 07:43:15 -0400

    drm: add atomic fxns
    The 'atomic' mechanism allows for multiple properties to be updated,
    checked, and commited atomically.  This will be the basis of atomic-
    modeset and nuclear-pageflip.
    
    The basic flow is:
    
       state = dev->atomic_begin();
       for (... one or more ...)
          obj->set_property(obj, state, prop, value);
       if (dev->atomic_check(state))
          dev->atomic_commit(state);
       dev->atomic_end(state);
    
    The split of check and commit steps is to allow for ioctls with a
    test-only flag (which would skip the commit step).
    
    Signed-off-by: Rob Clark <robdclark@gmail.com>

--- drivers/gpu/drm/i915/intel_crt.c
+++ drivers/gpu/drm/i915/intel_crt.c
@@ -725,8 +725,10 @@ out:
 }
 
 static int intel_crt_set_property(struct drm_connector *connector,
+				  struct drm_atomic_state *state,
 				  struct drm_property *property,
-				  uint64_t value)
+				  uint64_t value,
+				  void *blob_data)
 {
 	return 0;
 }
