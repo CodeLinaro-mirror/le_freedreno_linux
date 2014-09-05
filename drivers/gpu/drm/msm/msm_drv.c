8e343c1467af7b7ec302bef3547b0bbb34bd2d17
Rob Clark <robdclark@gmail.com>
2014-09-05 13:06:37 -0400

    drm/msm/adreno: split adreno device out into it's own file
    We'd rather not duplicate these parts as support for additional gpu
    generations is added.
    
    Signed-off-by: Rob Clark <robdclark@gmail.com>

--- drivers/gpu/drm/msm/msm_drv.c
+++ drivers/gpu/drm/msm/msm_drv.c
@@ -1025,7 +1025,7 @@ static int __init msm_drm_register(void)
 {
 	DBG("init");
 	hdmi_register();
-	a3xx_register();
+	adreno_register();
 	return platform_driver_register(&msm_platform_driver);
 }
 
@@ -1034,7 +1034,7 @@ static void __exit msm_drm_unregister(void)
 	DBG("fini");
 	platform_driver_unregister(&msm_platform_driver);
 	hdmi_unregister();
-	a3xx_unregister();
+	adreno_unregister();
 }
 
 module_init(msm_drm_register);
