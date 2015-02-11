6c1dc0b28eb9d85fc75c2ca425b1ad799ced7994
Rob Clark <robdclark@gmail.com>
2015-02-10 23:14:12 -0500

    Revert "clk: qcom: gcc8960: remove temporary rpm fixed clocks"
    This reverts commit 495a3e309e32a01c1a46c9a591d7811e7490ae7c.
    
    Conflicts:
    	drivers/clk/qcom/gcc-msm8960.c

--- drivers/clk/qcom/gcc-msm8960.c
+++ drivers/clk/qcom/gcc-msm8960.c
@@ -3656,6 +3656,8 @@ MODULE_DEVICE_TABLE(of, gcc_msm8960_match_table);
 
 static int gcc_msm8960_probe(struct platform_device *pdev)
 {
+	struct clk *clk;
+	struct device *dev = &pdev->dev;
 	const struct of_device_id *match;
 
 	match = of_match_device(gcc_msm8960_match_table, &pdev->dev);
@@ -3667,8 +3669,16 @@ static int gcc_msm8960_probe(struct platform_device *pdev)
 		hfpll_l2.d = &hfpll_l2_8064_data;
 	}
 
-	qcom_cc_probe(pdev, match->data);
-	return of_platform_populate(pdev->dev.of_node, NULL, NULL, &pdev->dev);
+	/* Temporary until RPM clocks supported */
+	clk = clk_register_fixed_rate(dev, "cxo", NULL, CLK_IS_ROOT, 19200000);
+	if (IS_ERR(clk))
+		return PTR_ERR(clk);
+
+	clk = clk_register_fixed_rate(dev, "pxo", NULL, CLK_IS_ROOT, 27000000);
+	if (IS_ERR(clk))
+		return PTR_ERR(clk);
+
+	return qcom_cc_probe(pdev, match->data);
 }
 
 static int gcc_msm8960_remove(struct platform_device *pdev)
