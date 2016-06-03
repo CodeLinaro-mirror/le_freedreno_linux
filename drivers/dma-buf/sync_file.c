a8e693acc7e753fa6453c30931322bd258c95920
Gustavo Padovan <gustavo.padovan@collabora.co.uk>
2016-06-03 19:58:37 -0300

    Revert "dma-buf/sync_file: only enable fence signalling during wait"
    This reverts commit 4df498de73fb1810b1c7b42c8fff7ad785eff1ea.

--- drivers/dma-buf/sync_file.c
+++ drivers/dma-buf/sync_file.c
@@ -92,7 +92,9 @@ struct sync_file *sync_file_create(struct fence *fence)
 
 	sync_file->cbs[0].fence = fence;
 	sync_file->cbs[0].sync_file = sync_file;
-	INIT_LIST_HEAD(&sync_file->cbs[0].cb.node);
+	if (fence_add_callback(fence, &sync_file->cbs[0].cb,
+			       fence_check_cb_func))
+		atomic_dec(&sync_file->status);
 
 	sync_file_debug_add(sync_file);
 	return sync_file;
@@ -186,7 +188,8 @@ static void sync_file_add_pt(struct sync_file *sync_file, int *i,
 	sync_file->cbs[*i].fence = fence;
 	sync_file->cbs[*i].sync_file = sync_file;
 
-	if (!fence_is_signaled(fence)) {
+	if (!fence_add_callback(fence, &sync_file->cbs[*i].cb,
+				fence_check_cb_func)) {
 		fence_get(fence);
 		(*i)++;
 	}
@@ -270,8 +273,11 @@ static void sync_file_free(struct kref *kref)
 	if (sync_file->fence_array)
 		fence_put(&sync_file->fence_array->base);
 
-	for (i = 0; i < sync_file->num_fences; ++i)
+	for (i = 0; i < sync_file->num_fences; ++i) {
+		fence_remove_callback(sync_file->cbs[i].fence,
+				      &sync_file->cbs[i].cb);
 		fence_put(sync_file->cbs[i].fence);
+	}
 
 	sync_file_debug_remove(sync_file);
 	kfree(sync_file);
@@ -288,33 +294,17 @@ static int sync_file_release(struct inode *inode, struct file *file)
 static unsigned int sync_file_poll(struct file *file, poll_table *wait)
 {
 	struct sync_file *sync_file = file->private_data;
-	int status, i;
+	int status;
 
 	poll_wait(file, &sync_file->wq, wait);
 
-	for (i = 0 ; i < sync_file->num_fences ; i++) {
-		if (fence_is_enabled(sync_file->cbs[i].fence))
-			continue;
-
-		if (fence_add_callback(sync_file->cbs[i].fence,
-				       &sync_file->cbs[i].cb,
-				       fence_check_cb_func))
-			atomic_dec(&sync_file->status);
-	}
-
 	status = atomic_read(&sync_file->status);
 
-	if (status > 0)
-		return 0;
-
-	for (i = 0 ; i < sync_file->num_fences ; i++)
-		fence_remove_callback(sync_file->cbs[i].fence,
-				      &sync_file->cbs[i].cb);
-
+	if (!status)
+		return POLLIN;
 	if (status < 0)
 		return POLLERR;
-
-	return POLLIN;
+	return 0;
 }
 
 static long sync_file_ioctl_merge(struct sync_file *sync_file,
